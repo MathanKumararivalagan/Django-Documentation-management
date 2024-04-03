34609b5bdcf27826b7213e531728edc50deeabc8
Mathan Kumar A <155807290+MathanKumararivalagan@users.noreply.github.com>
2024-04-04 00:14:20 +0530

    Delete Reader/Abstract_Reader directory

--- Reader/Abstract_Reader/Reader/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-# Register your models here.
-
-from .models import *
-# Register your models here.
-admin.site.register(Student)
